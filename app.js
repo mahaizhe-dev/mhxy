5219f81c372ba3f733220e39cf8fda3ed9048066
mahaizhe-dev <mahaizhe@gmail.com>
2026-08-01 12:12:31 +0800

    Emergency restore public site runtime

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-TEMP
+window.PUBLIC_SNAPSHOT = 'snapshot.9b700fb885085195.json';
+const restoredScript = document.createElement('script');
+restoredScript.src = 'https://raw.githubusercontent.com/mahaizhe-dev/mhxy/b6225a80da20307910aafbd203a3d0af87657484/app.js';
+restoredScript.onerror = () => document.querySelector('#toast')?.classList.add('show');
+document.head.append(restoredScript);
